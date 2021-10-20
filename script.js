51afb92535d6e29a7e5b8fe3fe0daf6b43bb809c
tharsila <tharsilapaula@gmail.com>
2021-10-20 16:40:26 -0300

    add function tabela

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const personagensContador= document.getElementById('personagens');
-const luasContador= document.getElementById('luas');
-const planetasContador= document.getElementById('planetas');
-const navesContador= document.getElementById('naves');
+const personagensContador = document.getElementById('personagens');
+const luasContador = document.getElementById('luas');
+const planetasContador = document.getElementById('planetas');
+const navesContador = document.getElementById('naves');
 
 preencherContadores();
+preencherTabela();
 
 function preencherContadores() {
-    Promise.all([swapiGet('people/'), swapiGet('vehicles/'), swapiGet('planets/'), swapiGet('starships/')]).then(function (results) {
-    personagensContador.innerHTML = results[0].data.count;
-    luasContador.innerHTML = results[1].data.count;
-    planetasContador.innerHTML = results[2].data.count;
-    navesContador.innerHTML = results[3].data.count;
-  });
+    Promise.all([
+      swapiGet('people/'), 
+      swapiGet('vehicles/'), 
+      swapiGet('planets/'), 
+      swapiGet('starships/')
+    ]).then(function (results) {
+      personagensContador.innerHTML = results[0].data.count; 
+      luasContador.innerHTML = results[1].data.count; 
+      planetasContador.innerHTML = results[2].data.count; 
+      navesContador.innerHTML = results[3].data.count;});
+}
+
+async function preencherTabela () {
+ const response = await swapiGet('films/');
+ console.log(response);
 }
 
 
